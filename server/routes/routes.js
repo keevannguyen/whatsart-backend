d3b9de1dd1238538980800eeda1714647daa31bb
kevinnguyen125 <kevinnguyen125@gmail.com>
2018-09-05 12:45:45 -0700

    Allow for an array of queries to POST artwork

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -36,29 +36,37 @@ router.post('/artwork', (req, res, next) => {
   if (!req.body.artworkName) {
     return res.status(404).send('No Artwork Name Provided.');
   } else {
-    let artworkInfoOutter = null;
-    getWikiInfo(req.body.artworkName)
-    .then((info) => {
-      const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-      const geocodeURL = `${baseURL}${accents.remove(info.museum).split(" ").join("+")}+` +
-                         `${accents.remove(info.city).split(" ").join("+")}&key=${process.env.GOOGLE_MAPS_GEOCODE_API_KEY}`;
-      artworkInfoOutter = { ...info, dateViewed: new Date() };
-      return axios.get(geocodeURL);
-    })
-    .then((resp) => {
-      const { lat, lng } = resp.data.results[0].geometry.location;
-      artworkInfoOutter = { ...artworkInfoOutter, lat: lat, lng: lng };
-      return Artwork.findOrCreate({ title: artworkInfoOutter.title });
-    })
-    .then(({ doc }) => {
-      return Object.assign(doc, artworkInfoOutter).save();
-    })
-    .then((artwork) => {
-      //req.user.userCollection.push(artwork._id);
-      //req.user.save();
-      res.json({ success: true, artworkInfo: artwork });
-    })
-    .catch(err =>{ console.log(err); res.status(404).json({ success: false, error: err }) });
+    console.log(req.body.artworkName);
+    for (let i = 0; i < req.body.artworkName.length; ++i) {
+      let artworkInfoOutter = null;
+      getWikiInfo(req.body.artworkName[i])
+      .then((info) => {
+        const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+        const geocodeURL = `${baseURL}${accents.remove(info.museum).split(" ").join("+")}+` +
+                           `${accents.remove(info.city).split(" ").join("+")}&key=${process.env.GOOGLE_MAPS_GEOCODE_API_KEY}`;
+        artworkInfoOutter = { ...info, dateViewed: new Date() };
+        return axios.get(geocodeURL);
+      })
+      .then((resp) => {
+        const { lat, lng } = resp.data.results[0].geometry.location;
+        artworkInfoOutter = { ...artworkInfoOutter, lat: lat, lng: lng };
+        return Artwork.findOrCreate({ title: artworkInfoOutter.title });
+      })
+      .then(({ doc }) => {
+        return Object.assign(doc, artworkInfoOutter).save();
+      })
+      .then((artwork) => {
+        //req.user.userCollection.push(artwork._id);
+        //req.user.save();
+        res.json({ success: true, artworkInfo: artwork });
+      })
+      .catch(err =>{
+        console.log(err);
+        if(i === req.body.artworkName.length-1) {
+          res.status(404).json({ success: false, error: err, msg: 'Could not find artwork.' });
+        }
+      });
+    }
   }
 });
 
