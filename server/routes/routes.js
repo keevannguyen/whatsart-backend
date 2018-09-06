ba4b85d6b3c2fa8be961d7368bebc82c30161ab4
kevinnguyen125 <kevinnguyen125@gmail.com>
2018-09-05 23:22:40 -0700

    Signifcant Algorithm Upgrade: Uses Wikipedia Search Query for more success

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -33,56 +33,75 @@ router.get('/user', (req, res, next) => {
 
 // POST route for Users to add new Artworks
 router.post('/artwork', (req, res, next) => {
-  // axios.get("https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=5&srsearch=Lisa+del+Giocondo")
-  // .then((resp) => console.log(resp.data));
   if (!req.body.artworkName) {
     return res.status(404).send('No Artwork Name Provided.');
   } else {
-    const getArtworkInfo = (index = 0) => {
-      return new Promise((resolve, reject) => {
-        let artworkInfoOutter = null;
-        getWikiInfo(req.body.artworkName[index])
-        .then((info) => {
-          artworkInfoOutter = { ...info, dateViewed: new Date() };
-          if(info.museum && info.city) {
-            const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-            const geocodeURL = `${baseURL}${accents.remove(info.museum).split(" ").join("+")}+` +
-                               `${accents.remove(info.city).split(" ").join("+")}&key=${process.env.GOOGLE_MAPS_GEOCODE_API_KEY}`;
-            return axios.get(geocodeURL);
-          }
-          else {
-            return Promise.resolve({ lat: 'Not Available', lng: 'Not Available' });
-          }
-        })
+    const getArtworkInfoOutter = (outterIndex = 0) => {
+      return new Promise((outterResolve, outterReject) => {
+        const searchQuery = req.body.artworkName[outterIndex].split(" ").join("+");
+        axios.get(`https://en.wikipedia.org/w/api.php?format=json&action=query&list=search&srlimit=3&srsearch=${searchQuery}`)
         .then((resp) => {
-          if(resp.data) { resp = resp.data.results[0].geometry.location }
-          const { lat, lng } = resp;
-          artworkInfoOutter = { ...artworkInfoOutter, lat: lat, lng: lng };
-          return Artwork.findOrCreate({ title: artworkInfoOutter.title });
+          const possibleWikiPages = resp.data.query.search.map((obj)=>obj.title);
+          const getArtworkInfoInner = (innerIndex = 0) => {
+            return new Promise((innerResolve, innerReject) => {
+              let artworkInfoStored = null;
+              getWikiInfo(possibleWikiPages[innerIndex])
+              .then((info) => {
+                artworkInfoStored = { ...info, dateViewed: new Date() };
+                if(info.museum && info.city) {
+                  const baseURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
+                  const geocodeURL = `${baseURL}${accents.remove(info.museum).split(" ").join("+")}+` +
+                                     `${accents.remove(info.city).split(" ").join("+")}&key=${process.env.GOOGLE_MAPS_GEOCODE_API_KEY}`;
+                  return axios.get(geocodeURL);
+                }
+                else {
+                  return Promise.resolve({ lat: 'Not Available', lng: 'Not Available' });
+                }
+              })
+              .then((resp) => {
+                if(resp.data) { resp = resp.data.results[0].geometry.location }
+                const { lat, lng } = resp;
+                artworkInfoStored = { ...artworkInfoStored, lat: lat, lng: lng };
+                return Artwork.findOrCreate({ title: artworkInfoStored.title });
+              })
+              .then(({ doc }) => {
+                return Object.assign(doc, artworkInfoStored).save();
+              })
+              .then((artwork) => {
+                //req.user.userCollection.push(artwork._id);
+                //req.user.save();
+                return innerResolve(artwork);
+              })
+              .catch(err => {
+                return innerReject(err);
+              });
+            })
+            .then((artwork) => Promise.resolve(artwork))
+            .catch((err) => {
+              if(innerIndex === possibleWikiPages.length-1) {
+                // console.log(err);
+                return Promise.reject(err);
+              } else {
+                return getArtworkInfoInner(++innerIndex);
+              }
+            });
+          };
+          return getArtworkInfoInner();
         })
-        .then(({ doc }) => {
-          return Object.assign(doc, artworkInfoOutter).save();
-        })
-        .then((artwork) => {
-          //req.user.userCollection.push(artwork._id);
-          //req.user.save();
-          return resolve(artwork);
-        })
-        .catch(err => {
-          return reject(err);
-        });
+        .then((artwork) => outterResolve(artwork))
+        .catch((err) => outterReject(err));
       })
       .then((artwork) => res.json({ success: true, artworkInfo: artwork }))
       .catch((err) =>{
-        console.log(err);
-        if(index === req.body.artworkName.length-1) {
+        // console.log(err);
+        if(outterIndex === req.body.artworkName.length-1) {
           res.status(404).json({ success: false, error: err, msg: 'Could not find artwork.' });
         } else {
-          getArtworkInfo(++index);
+          getArtworkInfoOutter(++outterIndex);
         }
       });
     };
-    getArtworkInfo();
+    getArtworkInfoOutter();
   }
 });
 
